d0f9768de9b6f63824f65e2810d160f90cc62f18
ArmanDipu <80636334+ArmanDipu@users.noreply.github.com>
2021-10-07 10:00:10 +0600

    Add files via upload

--- CartRemoveProduct.php
+++ CartRemoveProduct.php
@@ -11,6 +11,7 @@
 		$id = $_GET['deleteID'];
 		$sql = "delete From cart WHERE CartProductID = $id;" ;
     	$result = mysqli_query($dbCart,$sql);
+    	$user = $_SESSION["username"];
     	header("location:cart.php?user=$user");
 	}
 
